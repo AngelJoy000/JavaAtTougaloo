4a2f9a777d9c9310640561cd418c2ff614f46c0b
Angel Patterson <angel-patterson@hotmail.com>
2017-02-20 14:18:33 -0600

    Update lotteryAppDemo.java

--- lotteryAppDemo.java
+++ lotteryAppDemo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-/* @author Lemuel M. Uhuru
+/* @author Lemuel M. Uhuru and Angel Patterson
  * @Date   May 5, 2013
  * Book Title: From Control Structures through Objects by Tony Gaddis 
  * Page: 493 Problem 9
