96d740682e21ab1709eb5685824148d63d6531d4
Angel Patterson <angel-patterson@hotmail.com>
2017-02-20 14:17:46 -0600

    Update DistanceFile.java

--- DistanceFile.java
+++ DistanceFile.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.io.*;
 
 
-/**@author Lemuel M. Uhuru
+/**@author Lemuel M. Uhuru and Angel Patterson
  * @Date   June 23, 2013
  * Book Title: From Control Structures through Objects by Tony Gaddis 
  * Page: 267 Challenge 3: Distance File
